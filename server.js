1639de23aad9dfd7084ddeb26ec18cd40d6e3f3b
Maaz Khan <maazkhan7769@gmail.com>
2023-07-31 15:54:45 +0530

    custom middleware which logs events on the server

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require("express")
 const app = express()
 const path = require('path')
+const {logger} = require('./middleware/logger')
 const PORT = process.env.PORT || 3500
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(logger)
+
+app.use(express.json()) // parse the json from the request
+
+app.use('/', express.static(path.join(__dirname, '/public'))) // telling express where to look for static files
 
 app.use('/', require('./routes/root'))
 
+
+//if none of the routes matches we will return a 404 based on api whether it accepts the content (html/json)
 app.all('*', (req, res) => {
     res.status(404)
     if(req.accepts('html')){
         res.sendFile(path.join(__dirname, 'views', '404.html'))
     } else if(req.accepts('json')){
-        res.json({message : "40 Not Found"})
+        res.json({message : "404 Not Found"})
     } else {
         res.type('txt').send('404 Not Found')
     }
